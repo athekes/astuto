667bc312479c042bb78916768fee4368f93895b0
Gustavo <lgx.0097@gmail.com>
2022-12-13 18:53:25 -0300

    add user e comments tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,36 @@ FactoryBot.define do
     notifications_enabled { true }
     password { 'password' }
   end
+
+  factory :moderator, class: User do
+    sequence(:email) { |n| "mod#{n}@example.com" }
+
+    full_name { 'First Last' }
+    password { 'password' }
+    role { 'moderator' }
+  end
+
+  factory :admin, class: User do
+    sequence(:email) { |n| "admin#{n}@example.com" }
+
+    full_name { 'First Last' }
+    password { 'password' }
+    role { 'admin' }
+  end
+
+  factory :blocked, class: User do
+    sequence(:email) { |n| "admin#{n}@example.com" }
+
+    full_name { 'First Last' }
+    password { 'password' }
+    status { 'blocked' }
+  end
+
+  factory :deleted, class: User do
+    sequence(:email) { |n| "admin#{n}@example.com" }
+
+    full_name { 'First Last' }
+    password { 'password' }
+    status { 'deleted' }
+  end
 end
